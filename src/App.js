51f009a54f429dfb8420e6a594f2e92d1e8219fd
iamsuru <suryanshshrivastava6364@gmail.com>
2023-12-12 21:35:43 +0530

    0.1.1

--- src/App.js
+++ src/App.js
@@ -2,132 +2,172 @@ import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './App.css'
+import validator from 'validator'
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
 function App() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-  const [radio1, setRadio1] = useState('yes')
-  const [radio2, setRadio2] = useState('yes')
-  const [radio3, setRadio3] = useState('satisfied')
 
-  function sendSurvey(e) {
-    e.preventDefault();
-    //server side logic
-    console.log(`${name} ${email} ${message} ${radio1} ${radio2} ${radio3}`);
-    alert('Survey Sent.')
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [gender, setGender] = useState('');
+  const [email, setEmail] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [dob, setDob] = useState('');
+  const [country, setCountry] = useState('');
+  const [modal, setModal] = useState(false)
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (!firstName || !lastName || !email || !gender || !mobileNumber || !dob || !country) {
+      alert('All fields are required');
+      return;
+    }
+    if (!validator.isEmail(email)) {
+      alert('Invalid Email')
+      return;
+    }
+
+    if (!validator.isMobilePhone(mobileNumber)) {
+      alert('Invalid Number')
+      return
+    }
+    setModal(true)
+  };
+
+  function modalHandler() {
+    setModal(!modal)
   }
 
+  function handleReset() {
+    resetForm();
+    setModal(false)
+  };
+
+  function resetForm() {
+    setFirstName('');
+    setLastName('');
+    setGender('');
+    setEmail('');
+    setMobileNumber('');
+    setDob('');
+    setCountry('');
+  }
+
+
   return (
-    <div className='flex-container'>
-    <div className='mt-4 mb-3'>
-          <h1 className='heading'>Customer Survey Form</h1>
-        </div>
-      <div className='survey-form'>
-        <div className='mt-3'>
-          <form onSubmit={sendSurvey}>
-            <div className='row mt-3'>
-              <div className='col-sm-6'>
-                <div className="mb-3">
-                  <label htmlFor="name" className="form-label">Name</label>
-                  <input type="text" className="form-control" id="name" aria-describedby="emailHelp" onChange={(e) => setName(e.target.value)} />
-                </div>
-              </div>
-              <div className='col-sm-6'>
-                <div className="mb-3">
-                  <label htmlFor="email" className="form-label">Email</label>
-                  <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={(e) => setEmail(e.target.value)} />
-                </div>
-              </div>
-            </div>
-
-
-            <div className='mt-2 mb-2'>
-              <label>Is this first time you are using our products & service?</label><br></br>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio1" id="radio1-yes" value="yes" checked={radio1 === 'yes'} onChange={(e) => setRadio1(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio1-yes">Yes</label>
-              </div>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio1" id="radio1-no" value="no" checked={radio1 === 'no'} onChange={(e) => setRadio1(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio1-no">No</label>
-              </div>
-            </div>
-
-            <div className='mt-2 mb-2'>
-              <label>Would you suggest to your friends and colleague?</label><br></br>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio2" id="radio2-yes" value="yes" checked={radio2 === 'yes'} onChange={(e) => setRadio2(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio2-yes">Yes</label>
-              </div>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio2" id="radio2-no" value="no" checked={radio2 === 'no'} onChange={(e) => setRadio2(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio2-no">No</label>
-              </div>
-            </div>
-
-            <div className='mt-2 mb-2'>
-              <label>How satisfied are you with our company overall?</label><br></br>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio3" id="radio3-satisfied" value="satisfied" checked={radio3 === 'satisfied'} onChange={(e) => setRadio3(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio3-satisfied">Satisfied</label>
-              </div>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio3" id="radio3-undecided" value="undecided" checked={radio3 === 'undecided'} onChange={(e) => setRadio3(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio3-undecided">Undecided</label>
-              </div>
-              <div className="form-check form-check-inline mt-2">
-                <input className="form-check-input" type="radio" name="radio3" id="radio3-unsatisfied" value="unsatisfied" checked={radio3 === 'unsatisfied'} onChange={(e) => setRadio3(e.target.value)} />
-                <label className="form-check-label" htmlFor="radio3-unsatisfied">Unsatisfied</label>
-              </div>
-
-
-            </div>
-
-            <div className="form-group mt-2 mb-2">
-              <label htmlFor="message">Do you have suggestion to improve our service?</label>
-              <textarea className="form-control" id="message" rows="5" onChange={(e) => setMessage(e.target.value)}></textarea>
-            </div>
-
-            <button type="submit" className="btn mt-3">SUBMIT</button>
-          </form>
-        </div>
+    <>
+      <div className='container text-center mt-3'>
+        <h1>Survey Form</h1>
       </div>
-      <div className='mt-3'>
-          <span className='footer'>&copy;2023 Customer Survey Form. All Rights Reserved | Design by <a href='https://www.linkedin.com/in/iamsuru' rel='noreferrer noopener' target='_blank'>Suryansh Shrivastava</a> </span>
+      <div className='container'>
+        <div className='flex-container'>
+          <div className='survey-form'>
+            <form>
+              <div className="mb-3">
+                <label htmlFor="first_name" className='form-label'>First Name</label><span>*</span>
+                <input type="text" className='form-control' id="first_name" placeholder='Enter your First Name' value={firstName} onChange={function (e) { setFirstName(e.target.value) }} />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="last_name" className="form-label">Last Name</label><span>*</span>
+                <input type="text" className="form-control" id="last_name" placeholder='Enter your Surname' value={lastName} onChange={function (e) { setLastName(e.target.value) }} />
+              </div>
+
+              <div className='row'>
+                <div className='col-sm-12'>
+                  <label htmlFor=''>Gender : </label><span className='me-5'>*</span>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" id="male" name="gender" value="Male"
+                      checked={gender === 'Male'} onChange={function () { setGender('Male') }}
+                    />
+                    <label className="form-check-label" htmlFor="male">Male</label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" id="female" name="gender" value="Female"
+                      checked={gender === 'Female'} onChange={function () { setGender('Female') }}
+                    />
+                    <label className="form-check-label" htmlFor="female">Female</label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="radio" id="other" name="gender" value="Other"
+                      checked={gender === 'Other'} onChange={function () { setGender('Other') }}
+                    />
+                    <label className="form-check-label" htmlFor="other">Other</label>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+              <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email</label><span>*</span>
+                <input type="email" className="form-control" id="email" placeholder='Enter your Email' value={email} onChange={function (e) { setEmail(e.target.value) }} />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="mobile_number" className="form-label">Mobile Number</label><span>*</span>
+                <input type="text" className="form-control" id="mobile_number" placeholder='Enter your Number' value={mobileNumber} onChange={function (e) { setMobileNumber(e.target.value) }} />
+              </div>
+
+
+              <div className="mb-3">
+                <label htmlFor="dob" className="form-label">Date Of Birth</label><span>*</span>
+                <input type="date" className="form-control" id="dob" value={dob} onChange={function (e) { setDob(e.target.value) }} />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="country" className="form-label">Choose Country</label><span>*</span>
+                <select className='form-select' id='country' value={country} onChange={function (e) { setCountry(e.target.value) }} >
+                  <option selected>Choose Country</option>
+                  <option>India</option>
+                  <option>China</option>
+                  <option>Japan</option>
+                  <option>Indonesia</option>
+                  <option>Iran</option>
+                  <option>Italy</option>
+                  <option>USA</option>
+                  <option>Uganda</option>
+                  <option>UK</option>
+                  <option>Russia</option>
+                </select>
+              </div>
+              <button type="submit" className="btn btn-outline-success float-start" onClick={handleSubmit}>Submit</button>
+              <button type="reset" className="btn btn-outline-danger float-end" onClick={handleReset}>Reset</button>
+            </form>
+          </div>
+          <Modal centered={true} isOpen={modal} toggle={modalHandler} onClosed={handleReset}>
+            <ModalHeader toggle={modalHandler}>Data Sent.</ModalHeader>
+            <ModalBody>
+              <dl className="row">
+                <dt className="col-sm-4">First Name : </dt>
+                <dd className="col-sm-8">{firstName}</dd>
+
+                <dt className="col-sm-4">Last Name : </dt>
+                <dd className="col-sm-8">{lastName}</dd>
+
+                <dt className="col-sm-4">Gender : </dt>
+                <dd className="col-sm-8">{gender}</dd>
+
+                <dt className="col-sm-4">Email : </dt>
+                <dd className="col-sm-8">{email}</dd>
+
+                <dt className="col-sm-4">Mobile Number : </dt>
+                <dd className="col-sm-8">{mobileNumber}</dd>
+
+                <dt className="col-sm-4">Date of Birth : </dt>
+                <dd className="col-sm-8">{dob}</dd>
+
+                <dt className="col-sm-4">Country : </dt>
+                <dd className="col-sm-8">{country}</dd>
+
+              </dl>
+            </ModalBody>
+          </Modal>
         </div>
-    </div>
+        <footer className='text-center mt-2'>&copy;2023 Customer Survey Form. All Rights Reserved | Design by <a href='https://www.linkedin.com/in/iamsuru' rel='noreferrer noopener' target='_blank'>Suryansh Shrivastava</a> </footer>
+      </div>
+    </>
   )
 }
 
 export default App
-
-
-// import React, { useState } from 'react'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-// import './App.css'
-// function App() {
-//   const [name, setName] = useState('');
-//   const [email, setEmail] = useState('');
-//   const [message, setMessage] = useState('');
-//   const [radio1, setRadio1] = useState('yes')
-//   const [radio2, setRadio2] = useState('yes')
-//   const [radio3, setRadio3] = useState('satisfied')
-
-//   function sendSurvey(e) {
-//     e.preventDefault();
-//     //server side logic
-//     console.log(`${name} ${email} ${message} ${radio1} ${radio2} ${radio3}`);
-//     alert('Survey Sent.')
-//   }
-
-//   return (
-//     <div>
-//       <div className='container d-flex flex-column justify-content-center align-items-center'>
-        
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
